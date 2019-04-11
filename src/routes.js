997206dfeedf2dd40baeba514feabbf7a0342d73
Susan Adelokiki <susan.adelokiki@andela.com>
2019-04-11 19:29:24 +0100

    Minor updates

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
-import Home from './containers/home';
-import CreateNote from './containers/createnote';
+import Home from './components/home';
+import CreateNote from './components/createnote';
 
 
 export default class NotesRoutes extends Component {
   render() {
     return (
         <div className="routes">
-          {/* <Home/> */}
-          <Route exact path='/home' component={Home} />
+          <Route exact path='/' component={Home} />
           <Route exact path='/notes' component={CreateNote} />
           <Route exact path='/notes/:id' component={ CreateNote } />
       </div>
